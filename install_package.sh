2479fce9276a7f8f501a4fe3b404db058dda7e44
蔡建峰 <84452200+cai-jianfeng@users.noreply.github.com>
2023-05-02 16:47:14 +0800

    Update install_package.sh
    reproduce ACTOR using mindspore_rc1

--- install_package.sh
+++ install_package.sh
@@ -1,11 +1,4 @@
-conda activate mindspore
-pip install tensorboard
-pip install matplotlib
-pip install ipdb
-pip install sklearn
-pip install pandas
-pip install tqdm
-pip install imageio
-pip install pyyaml
-pip install smplx
-pip install chumpy
+conda env create -f environment.yml
+conda activate mindspore_rc1
+export MS_VERSION=2.0.0rc1
+pip install https://ms-release.obs.cn-north-4.myhuaweicloud.com/${MS_VERSION}/MindSpore/unified/x86_64/mindspore-${MS_VERSION/-/}-cp38-cp38-linux_x86_64.whl --trusted-host ms-release.obs.cn-north-4.myhuaweicloud.com -i https://pypi.tuna.tsinghua.edu.cn/simple
